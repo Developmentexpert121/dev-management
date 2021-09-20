78b4940f56b5dd1616af9de10c3db15b80024d19
Developmentexpert121 <developmentexpert121@gmail.com>
2021-09-20 18:40:07 +0530

    sprint section

---
+++ Modules/Projects/Resources/views/team/single_sprint.blade.php
@@ -0,0 +1,20 @@
+@include('projects::team.header')
+
+<div class="main-panel">    
+
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+              bghbnfhnhfbfjb
+              
+    
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+  @include('projects::team.footer')
